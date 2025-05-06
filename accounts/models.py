49edd96c0b85b15a4c6ee15214c7b1ce3797228f
kelvinmaringire <kelvinmaringire@gmail.com>
2025-05-06 12:10:32 +0200

    06-05-25

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.gis.db import models as postgis_models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+
 from wagtail.images.models import Image
 
 
@@ -19,3 +21,9 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    def save(self, *args, **kwargs):
+        # Hash password if it's in plain text
+        if not self.password.startswith('pbkdf2_sha256$'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
